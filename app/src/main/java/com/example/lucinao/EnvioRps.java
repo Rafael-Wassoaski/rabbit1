4b08df17f4f3a0304838c3fbba0ed32536328e97
Aluno ADS <meireles200@hotmail.com>
2019-09-11 21:23:51 -0300

    mais1

--- app/src/main/java/com/example/lucinao/EnvioRps.java
+++ app/src/main/java/com/example/lucinao/EnvioRps.java
@@ -6,15 +6,17 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeoutException;
 
-public class EnvioRps {
+public class EnvioRps implements Runnable{
 
-    private final static String QUEUE_NAME = "luciano";
+    private final static String QUEUE_NAME = "rafael";
     private final static String VHOST = "/";
     public final static String USER = "guest";
     public final static String PASSWD = "guest";
@@ -22,64 +24,40 @@ public class EnvioRps {
     private BlockingDeque<String> queue = new LinkedBlockingDeque <String>();
     private ConnectionFactory factory = new ConnectionFactory();
 
+    private String msg;
+    private String receptor;
 
-    void publishMessage(String message) {
+
+    public EnvioRps(String msg, String receptor){
+        this.msg = msg;
+        this.receptor = receptor;
+    }
+
+
+    @Override
+    public void run() {
+        factory.setHost(HOST);
+
+        factory.setUsername(USER);
+
+        factory.setPassword(PASSWD);
+        factory.setVirtualHost(VHOST);
         try {
-            Log.d("","[q] " + message);
-            queue.putLast(message);
-        } catch (InterruptedException e) {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(QUEUE_NAME, false, false,false, null);
+            msg = "["+receptor+"] "+ msg;
+            channel.basicPublish("",  QUEUE_NAME, null, msg.getBytes("UTF-8"));
+            Log.d("Confirma", "Msg foi");
+            channel.close();
+            connection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
             e.printStackTrace();
         }
-    }
 
 
-    private void setupConnectionFactory() {
-        String uri = "CLOUDAMQP_URL";
-        try {
-            factory.setAutomaticRecoveryEnabled(false);
-            factory.setUri(uri);
-        } catch (KeyManagementException | NoSuchAlgorithmException | URISyntaxException e1) {
-            e1.printStackTrace();
-        }
-    }
-
-
-    public void publishToAMQP()
-    {
-      Thread publishThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(true) {
-                    try {
-                        Connection connection = factory.newConnection();
-                        Channel ch = connection.createChannel();
-                        ch.confirmSelect();
-
-                        while (true) {
-                            String message = queue.takeFirst();
-                            try{
-                                ch.basicPublish("amq.fanout", "chat", null, message.getBytes());
-                                Log.d("", "[s] " + message);
-                                ch.waitForConfirmsOrDie();
-                            } catch (Exception e){
-                                Log.d("","[f] " + message);
-                                queue.putFirst(message);
-                                throw e;
-                            }
-                        }
-                    } catch (InterruptedException e) {
-                        break;
-                    } catch (Exception e) {
-                        Log.d("", "Connection broken: " + e.getClass().getName());
-                        try {
-                            Thread.sleep(5000); //sleep and then try again
-                        } catch (InterruptedException e1) {
-                            break;
-                        }
-                    }
-                }
-            }
-        });
-        publishThread.start();
     }
 }
